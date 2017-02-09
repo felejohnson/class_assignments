435906b484d45ab90378371405cb82790b59fdf3
felejohnson <feliciaj61@gmail.com>
2017-02-08 20:48:57 -0500

    Add files via upload

---
+++ Pizza.py
@@ -0,0 +1,15 @@
+A=int(input ("enter number: "))
+if A % 2==0:
+ print ("your number will be even")
+else:
+    print ("your number will be odd" )
+
+
+Name=(input("What is your name "))
+print ("Nice to meet you", Name)
+
+Color=(input("What is your favorite color"))
+print ("That's a nice color",Color )'''
+
+Food=(input("What is your favorite food"))
+print (Food, "sounds yummy" )
